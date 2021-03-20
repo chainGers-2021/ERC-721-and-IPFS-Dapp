bd5985bad1cef5b13c41ab8fd2c35542fe229d2d
shantanhunt <shantanusontakke015@gmail.com>
2021-03-20 05:58:44 -0700

    File is uploading on IPFS

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,47 @@
  import React, { Component } from 'react';
-import logo from '../logo.jpg';
+import logo from '../logo.png';
 import './App.css';
 
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' })
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      buffer:null
+    };
+
+  }
+
+
   captureFile = (event) => {
     event.preventDefault()
     console.log('file captured ...')
 
     const file = event.target.files[0]
+    console.log('File is ',file)
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      console.log('buffer', Buffer(reader.result))
+      this.setState({'buffer': Buffer(reader.result)})
+    }
 
   }
+
+  onSubmit = async(event) => {
+    event.preventDefault()
+    console.log('Submitting the file...')
+    //const result = await ipfs.add(this.state.buffer)
+    //console.log(result)
+    for await (const r of ipfs.addAll(this.state.buffer)) {
+      console.log(r)
+    }
+  }
+
+
 
   render() {
     return (
@@ -25,7 +57,7 @@ class App extends Component {
                   <img src={logo} className="App-logo" alt="logo" />
                 <p> &nbsp; </p>
                 <h2> Change Picture </h2>
-                <form>
+                <form onSubmit = {this.onSubmit}>
                   <input type = 'file' onChange = {this.captureFile} />
                   <input type = 'submit' />
                 </form>
