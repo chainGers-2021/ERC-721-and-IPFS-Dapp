4727da461229ef06e66cab0e8ada6e605769add6
shantanhunt <shantanusontakke015@gmail.com>
2021-03-26 07:46:06 -0700

    Anupam contributed

--- src/components/App.js
+++ src/components/App.js
@@ -59,8 +59,7 @@ class App extends Component {
       window.web3 = new Web3(window.ethereum)
       await window.ethereum.enable()
     } if(window.web3){
-      window.web3 = new Web3(window.web3.currentProvider)
-
+      await window.web3.currentProvider.enable()
     } else{
       window.alert('Please use Metamask!')
     }
--- test/test.js
+++ test/test.js
@@ -35,12 +35,25 @@ require('chai')
 
 
 		describe('mintNFT', async() =>{
-			it('Minting the First NFT', async() => {
+			it('Minting an NFT and getting its IPFS hash', async() => {
 				let recipient = "0x1292EAb55cF30D93cdC96Ff87327d2Bf6A438e68"
-				let hash = "QmUEeRYpH4uUBQCSi5r7emM3L139mr3DqbhQ6pDz5PHMKB"
-				let metaData = 'Bulbasaur'
-				const newItemId = (await file.awardItem.call(recipient, hash, metaData)).toNumber()
-				assert.equal(newItemId, 1)
+				let r2 = "0xdB8944c1c40209446074d4AC0c8F8909fdd60928"
+				let hash1 = "QmUEeRYpH4uUBQCSi5r7emM3L139mr3DqbhQ6pDz5PHMKB"
+				let metaData1 = 'Bulbasaur1'
+				const newItemId1 = (await file.awardItem.call(recipient, hash1, metaData1)).toNumber()
+				assert.equal(newItemId1, 1)
+				var gethash = await file.get()
+				assert.equal(gethash, hash1)
+				// let recipient = "0x1292EAb55cF30D93cdC96Ff87327d2Bf6A438e68"
+				// let r2 = "0xdB8944c1c40209446074d4AC0c8F8909fdd60928"
+				// let hash1 = "QmUEeRYpH4uUBQCSi5r7emM3L139mr3DqbhQ6pDz5PHMKB"
+				// let hash2 = "QmTTS84W8iuYuH9czknSjZdbsEbSJPWxUYFJx8w8kUvuYX"
+				// let metaData1 = 'Bulbasaur1'
+				// let metaData2 = 'Bulbasaur2'
+				// const newItemId1 = (await file.awardItem.call(recipient, hash1, metaData2)).toNumber()
+				// assert.equal(newItemId1, 1)
+				// const newItemId2 = (await file.awardItem.call(r2, hash2, metaData2)).toNumber()
+				// assert.equal(newItemId2, 2)
 			})
 		})
 
